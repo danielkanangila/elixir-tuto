51aea0baab3a2bf0281166c5a47d1038d8252dcf
Daniel Kanangila <dkanangila@paystand.com>
2022-01-04 12:16:47 -0800

    update cards app

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,8 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    deck_lentgh = length(Cards.create_deck)
+    assert deck_lentgh == 20
   end
 end
